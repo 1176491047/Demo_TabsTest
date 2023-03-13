e687e3e1bafd96ecc55b3c6392c374f4f0a1f552
jiaqi <1176491047@qq.com>
2023-03-13 13:42:05 +0800

    添加项目文件。

---
+++ Pages/PrintTime.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Demo_TabsTest.Pages
+{
+    public partial class PrintTime
+    {
+        [Parameter]
+        public string text { get; set; }
+
+
+        DateTime currentTime = DateTime.Now;
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            currentTime = DateTime.Now;
+        }
+    }
+}
---
+++ Shared/MainLayout.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace Demo_TabsTest.Shared
+{
+    public partial class MainLayout
+    {
+
+        string para_text = "初始值";
+        public async Task OnTabClick()
+        {
+            para_text = "重新赋值";
+            await InvokeAsync(StateHasChanged);
+        }
+    }
+}
